32734e8765bbc3311cd8edce4d56f8bd100ff581
rami3l <rami3l@outlook.com>
2024-01-22 19:39:21 +0800

    refactor(cli): rewrite `rustup (man|completions)` with `clap-derive`

--- tests/suite/cli_misc.rs
+++ tests/suite/cli_misc.rs
@@ -798,11 +798,11 @@ fn completion_bad_shell() {
     setup(&|config| {
         config.expect_err(
             &["rustup", "completions", "fake"],
-            r#"error: invalid value 'fake' for '[shell]'"#,
+            r#"error: invalid value 'fake' for '<SHELL>'"#,
         );
         config.expect_err(
             &["rustup", "completions", "fake", "cargo"],
-            r#"error: invalid value 'fake' for '[shell]'"#,
+            r#"error: invalid value 'fake' for '<SHELL>'"#,
         );
     });
 }
@@ -812,7 +812,7 @@ fn completion_bad_tool() {
     setup(&|config| {
         config.expect_err(
             &["rustup", "completions", "bash", "fake"],
-            r#"error: invalid value 'fake' for '[command]'"#,
+            r#"error: invalid value 'fake' for '[COMMAND]'"#,
         );
     });
 }
